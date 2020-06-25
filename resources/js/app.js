260f29918585fdc2a1f72515e54b8a0a09c2e9ad
Yukihiro Saito <y-saito@m-craft.com>
2020-06-25 17:30:15 +0900

    タスク詳細コンポーネント実装

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router';
 import HeaderComponent from "./components/HeaderComponent";
 import TaskListComponent from "./components/TaskListComponent";
+import TaskShowComponent from "./components/TaskShowComponent";
 
 /**
  * First we will load all of this project's JavaScript dependencies which
@@ -21,6 +22,12 @@ const router = new VueRouter({
       path: '/tasks',
       name: 'task.list',
       component: TaskListComponent
+    },
+    {
+      path: '/tasks/:taskId',
+      name: 'task.show',
+      component: TaskShowComponent,
+      props: true
     },
   ]
 });
