9cc78874ec56dd8ad85f3ba857e4a0cd0770b1e5
Yukihiro Saito <y-saito@m-craft.com>
2020-06-30 10:19:27 +0900

    タスク削除API実装

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::get('/tasks', 'TaskController@index');
 Route::post('/tasks', 'TaskController@store');
 Route::get('/tasks/{task}', 'TaskController@show');
 Route::put('/tasks/{task}', 'TaskController@update');
+Route::delete('/tasks/{task}', 'TaskController@destroy');
